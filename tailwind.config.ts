b1de98abeb8c75a30aac398d602bd142a17114f7
SHIVA NATH <nathshiva907@gmail.com>
2024-10-13 19:00:54 +0530

    second commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+// tailwind.config.js
+
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    
   ],
   theme: {
     extend: {
@@ -16,4 +19,14 @@ const config: Config = {
   },
   plugins: [],
 };
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
 export default config;
